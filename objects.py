8135019828064ed7a5fb8131e3df1d74636352eb
k.martynski <43373840+kmartynski@users.noreply.github.com>
2018-12-29 17:37:49 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,25 +1,16 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 class Charts:
-    def __init__(self, x, y, label):
+    def __init__(self, x, y, linecolor):
         self.x = x
         self.y = y
-        self.label = label
+        self.linecolor = linecolor
 
     def line_chart(self):
-        return plt.plot(self.x, self.y, label=self.label)
+        return plt.plot(self.x, self.y, color=self.linecolor)
 
     def pie_chart(self):
         return plt.pie(self.x, self.y)
 
     def column_chart(self):
         return plt.bar(self.x, self.y)
-
-
-e = np.linspace(10, 100)
-d = np.linspace(1, 1000)
-
-p = Charts(e**2, d, label='test data')
-p.line_chart()
-plt.legend(loc='upper right', frameon=False)
