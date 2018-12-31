d7aad0f642d1dccec3dc6087a1a58359b1e4ac63
ibm <dawid.figiel@gmail.com>
2018-12-31 18:08:25 +0100

    introduce templates, fix pie chart, refactoring, fix radio inputs, close plt io

--- main.py
+++ main.py
@@ -1,52 +1,48 @@
 from flask import Flask, render_template, request
-import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 import numpy as np
 import io
 import base64
-import pandas as pd
 from objects import *
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def starting_page():
     return render_template("index.html")
 
-@app.route('/chart')
+
+@app.route('/chart', methods=['GET', 'POST'])
 def charts():
+    plot_url = ''
     if request.method == 'POST':
-        beginning_x = request.form['x1']
-        ending_x = request.form['x2']
-        beginning_y = request.form['y1']
-        ending_y = request.form['y2']
-        multiplier = request.form['Z']
+
+        beginning_x = request.form.get('x1', '1')
+        ending_x = request.form.get('x2', '50')
+        beginning_y = request.form.get('y1', '1')
+        ending_y = request.form.get('y2', '50')
+        multiplier = request.form.get('Z', '1')
+        color = request.form.get('color', 'pink')
 
         x = np.linspace(int(beginning_x), int(ending_x))
         y = np.linspace(int(beginning_y), int(ending_y))
 
         img = io.BytesIO()
-        if request.form.get('czerwony'):
-            curvecolor = 'red'
-        elif request.form.get('zielony'):
-            curvecolor = 'green'
-        elif request.form.get('blue'):
-            curvecolor = 'blue'
-        else:
-            curvecolor = 'black'
 
-        line_plot = Charts(x, y ** int(multiplier), curvecolor)
+        line_plot = Charts(x, y ** int(multiplier), color)
         line_plot.line_chart()
         plt.savefig(img, format='png')
         img.seek(0)
         plot_url = base64.b64encode(img.getvalue()).decode()
+        plt.close()   # if you not close then next chart will be merged with the previous ones
+        # check: https://technovechno.com/creating-graphs-in-python-using-matplotlib-flask-framework-pythonanywhere/
+    return render_template('linechart.html', image=plot_url)
 
-        return '<img src="data:image/png;base64,{}">'.format(plot_url)
-    else:
-        return render_template('linechart.html')
 
 @app.route('/pie', methods=['GET', 'POST'])
 def pie_charts():
+    pie_plot_url = ''
     if request.method == 'POST':
         group1 = request.form['g1']
         group2 = request.form['g2']
@@ -64,17 +60,16 @@ def pie_charts():
         plt.savefig(pie_pie, format='png')
         pie_pie.seek(0)
         pie_plot_url = base64.b64encode(pie_pie.getvalue()).decode()
+        plt.close()
+    return render_template('piechart.html', image=pie_plot_url)
 
-        return '<img src="data:pie_pie/png;base64,{}">'.format(pie_plot_url)
-    else:
-        return render_template('piechart.html')
 
 @app.route('/column')
 def column_charts():
     img = io.BytesIO()
 
-    x = np.arange(4)
-    money = [1.5e5, 2.5e6, 5.5e6, 2.0e7]
+    x = np.arange(5)
+    money = [1.5e5, 2.5e6, 5.5e6, 2.0e7, 3.0e6]
 
     def millions(x, pos):
         'The two args are the value and tick position'
@@ -84,17 +79,27 @@ def column_charts():
     fig, ax = plt.subplots()
     ax.yaxis.set_major_formatter(formatter)
     plt.bar(x, money)
-    plt.xticks(x, ('Bill', 'Fred', 'Mary', 'Sue'))
+    plt.xticks(x, ('Bill', 'Fred', 'Mary', 'Sue', 'John'))
     plt.savefig(img, format='png')
     img.seek(0)
-
     plot_url = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+    return render_template('columnchart.html', image=plot_url)
 
-    return '<img src="data:image/png;base64,{}">'.format(plot_url)
 
 @app.route('/csv')
 def csv_table():
-    return "Tutaj możesz wygenerować tabelę csv!"
+    return render_template('csv.html', csv="Tutaj możesz wygenerować tabelę csv!")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# other ways to create graphs:
+# https://blog.ruanbekker.com/blog/2017/12/14/graphing-pretty-charts-with-python-flask-and-chartjs/
+# https://technovechno.com/creating-graphs-in-python-using-matplotlib-flask-framework-pythonanywhere/
+# google: flask graph
+
+# Flask templates
+# http://flask.pocoo.org/docs/1.0/tutorial/templates/
